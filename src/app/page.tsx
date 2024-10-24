34a6eb445c1b9fc5232950771ae89ab1fa8d0015
Fernando Marca Magalhães <fernandomarca@hotmail.com>
2024-10-24 14:17:24 -0300

    lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 
 export default function Home() {
   return (
-    <h1>hello</h1>
+    <h1>LevVideo</h1>
   );
 }
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -4,4 +4,6 @@ const Video = forwardRef<HTMLVideoElement>((props, ref) => (
   <video ref={ref} autoPlay playsInline style={{ width: '300px', height: '300px' }} />
 ));
 
+Video.displayName = 'Video';
+
 export default Video;
